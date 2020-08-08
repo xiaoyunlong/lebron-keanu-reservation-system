0ce8282f3483ff1e0b7a6dc257246d8be1c107d5
jenkincstang <jenkincs@163.com>
2020-08-08 16:10:46 +0800

    [reservation-system][jenkin] feature: modify property file'

--- run.bat
+++ run.bat
@@ -1,8 +1,8 @@
-for /f "tokens=5 delims= " %%a in ('netstat -ano ^| findstr "8004" ^| findstr "LISTENING"') do set pid=%%a
+for /f "tokens=5 delims= " %%a in ('netstat -ano ^| findstr "8088" ^| findstr "LISTENING"') do set pid=%%a
 if not "%pid%" == "" (
   taskkill /f /PID %pid%
 ) else (
   rem echo Server is not running.
 )
 
-start /b java -jar d:\deploy\spring-boot-employee-0.0.1-SNAPSHOT.jar
+start /b java -jar d:\deploy\reservation-system-0.0.1-SNAPSHOT.jar
