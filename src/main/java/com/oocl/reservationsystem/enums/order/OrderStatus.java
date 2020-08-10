0c3c94581ede8a0c7bd988cc5d10746dd8cecbab
Sumtudou <1163015195@qq.com>
2020-08-10 22:20:52 +0800

    [#05][Jay,Caps] refactor: Change the orderstatus to be consistent with the front end

--- src/main/java/com/oocl/reservationsystem/enums/order/OrderStatus.java
+++ src/main/java/com/oocl/reservationsystem/enums/order/OrderStatus.java
@@ -2,8 +2,8 @@ package com.oocl.reservationsystem.enums.order;
 
 
 public class OrderStatus {
-    public static final String NOT_USED = "NOT_USED";
-    public static final String USED = "USED";
-    public static final String CANCELLED = "CANCELLED";
-    public static final String FINISHED = "FINISHED";
+    public static final String NOT_USED = "Reserved";
+    public static final String USED = "Parking";
+    public static final String CANCELLED = "Cancelled";
+    public static final String FINISHED = "Finished";
 }
