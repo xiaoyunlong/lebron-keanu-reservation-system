081e0bf27295806de45eac84ec6ecd56af908484
Sumtudou <1163015195@qq.com>
2020-08-10 19:40:22 +0800

    [reservation-system][Jay,Caps] feature:pass handle all order exception

--- src/main/java/com/oocl/reservationsystem/common/GlobalExceptionHandler.java
+++ src/main/java/com/oocl/reservationsystem/common/GlobalExceptionHandler.java
@@ -1,16 +1,51 @@
 package com.oocl.reservationsystem.common;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import com.oocl.reservationsystem.exception.order.OrderCancelFailException;
+import com.oocl.reservationsystem.exception.order.OrderNotFoundException;
+import com.oocl.reservationsystem.exception.order.OrderStatusErrorException;
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 /**
  * 全局异常处理
  */
 @ControllerAdvice
 public class GlobalExceptionHandler {
+
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ExceptionHandler({MethodArgumentNotValidException.class})
+    @ResponseBody
+    public List<String> handleMethodArgumentNotValidException(MethodArgumentNotValidException exception){
+        return exception.getBindingResult().getFieldErrors().stream()
+                .map(e -> e.getField()+":"+e.getDefaultMessage()).collect(Collectors.toList());
+    }
+
+    @ExceptionHandler(OrderNotFoundException.class)
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    @ResponseBody
+    String orderNotFoundException(){
+        return "order not found.";
+    }
+
+    @ExceptionHandler(OrderCancelFailException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ResponseBody
+    String orderCancelFailException(){
+        return "order cancel failed.";
+    }
+
+    @ExceptionHandler(OrderStatusErrorException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ResponseBody
+    String orderStatusErrorException(){
+        return "Order status error.";
+    }
 
 }
---
+++ src/main/java/com/oocl/reservationsystem/enums/order/OrderStatus.java
@@ -0,0 +1,9 @@
+package com.oocl.reservationsystem.enums.order;
+
+
+public class OrderStatus {
+    public static final String NOT_USED = "NOT_USED";
+    public static final String USED = "USED";
+    public static final String CANCELLED = "CANCELLED";
+    public static final String FINISHED = "FINISHED";
+}
