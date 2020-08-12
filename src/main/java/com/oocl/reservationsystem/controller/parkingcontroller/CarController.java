9d78efba66685db0e84ad0f39696ce9da7614411
jenkincstang <jenkincs@163.com>
2020-08-12 19:39:42 +0800

    [#026][jenkin] refactor: add responseStatus to comtroller method

--- src/main/java/com/oocl/reservationsystem/controller/parkingcontroller/CarController.java
+++ src/main/java/com/oocl/reservationsystem/controller/parkingcontroller/CarController.java
@@ -3,10 +3,12 @@ package com.oocl.reservationsystem.controller.parkingcontroller;
 import com.oocl.reservationsystem.dto.parkingdto.CarRequest;
 import com.oocl.reservationsystem.service.parkingservice.CarService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -20,11 +22,13 @@ public class CarController {
   }
 
   @PostMapping("/cars")
+  @ResponseStatus(HttpStatus.OK)
   public void getParkingLotById(@RequestBody CarRequest carRequest) {
     carService.saveCar(carRequest);
   }
 
   @DeleteMapping("/cars/{id}")
+  @ResponseStatus(HttpStatus.OK)
   public void getParkingLotById(@PathVariable("id") Integer id) {
     carService.deleteCar(id);
   }
--- src/main/java/com/oocl/reservationsystem/controller/parkingcontroller/ParkingController.java
+++ src/main/java/com/oocl/reservationsystem/controller/parkingcontroller/ParkingController.java
@@ -7,9 +7,11 @@ import com.oocl.reservationsystem.service.parkingservice.ParkingService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -34,11 +36,13 @@ public class ParkingController {
   }
 
   @GetMapping("/parkinglots/{id}/parkingpositions")
+  @ResponseStatus(HttpStatus.OK)
   public List<ParkingPosition> getParkingPositionsByParkinglotId(@PathVariable("id") Integer id) {
     return parkingService.findParkingPositionsByParkinglotId(id);
   }
 
   @GetMapping("/parkinglots/{id}")
+  @ResponseStatus(HttpStatus.OK)
   public ParkingLot getParkingLotById(@PathVariable("id") Integer id) {
     return parkingService.findParkingLotById(id);
   }
