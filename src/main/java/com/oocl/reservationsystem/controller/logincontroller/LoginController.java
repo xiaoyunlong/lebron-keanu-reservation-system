a3d9bb9718f760e5c1bb13cf3bfe9cc0f7db63e7
Ellie04162606 <1181140808@qq.com>
2020-08-12 15:12:55 +0800

    [story019][Ellie] feature: Implement the login function

--- src/main/java/com/oocl/reservationsystem/controller/logincontroller/LoginController.java
+++ src/main/java/com/oocl/reservationsystem/controller/logincontroller/LoginController.java
@@ -1,20 +1,29 @@
 package com.oocl.reservationsystem.controller.logincontroller;
 
 import com.oocl.reservationsystem.dto.logindto.LoginRequest;
+import com.oocl.reservationsystem.dto.logindto.LoginResponse;
 import com.oocl.reservationsystem.dto.logindto.RegisterRequest;
-import com.oocl.reservationsystem.dto.orderdto.OrderRequest;
+import com.oocl.reservationsystem.service.loginservice.LoginService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
-
-import javax.validation.Valid;
 
 @RestController
 public class LoginController {
 
-  @PostMapping("/login")
-  public void customerLogin(@RequestBody LoginRequest loginRequest) {
+  private final LoginService loginService;
 
+  @Autowired
+  public LoginController(LoginService loginService) {
+    this.loginService = loginService;
+  }
+
+  @PostMapping("/login")
+  @ResponseBody
+  public LoginResponse customerLogin(@RequestBody LoginRequest loginRequest) {
+    return loginService.getCustomerLoginRequest(loginRequest);
   }
 
   @PostMapping("/register")
--- src/main/java/com/oocl/reservationsystem/service/loginservice/impl/LoginServiceImpl.java
+++ src/main/java/com/oocl/reservationsystem/service/loginservice/impl/LoginServiceImpl.java
@@ -1,7 +1,42 @@
 package com.oocl.reservationsystem.service.loginservice.impl;
 
+import com.oocl.reservationsystem.dto.logindto.LoginRequest;
+import com.oocl.reservationsystem.dto.logindto.LoginResponse;
+import com.oocl.reservationsystem.entity.loginentity.Customer;
+import com.oocl.reservationsystem.enums.login.CustomerEnum;
+import com.oocl.reservationsystem.exception.login.CustomerNoFoundException;
+import com.oocl.reservationsystem.repository.loginrepository.CustomerRepository;
+import com.oocl.reservationsystem.repository.parkingrepository.CarRepository;
 import com.oocl.reservationsystem.service.loginservice.LoginService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
-public class LoginServiceImpl implements LoginService {}
+public class LoginServiceImpl implements LoginService {
+
+  @Autowired
+  private CustomerRepository customerRepository;
+
+  @Autowired
+  private CarRepository carRepository;
+
+  @Override
+  public LoginResponse getCustomerLoginRequest(LoginRequest loginRequest) {
+    List<Customer> customers = customerRepository
+        .findByEmailAndPassword(loginRequest.getEmail(), loginRequest.getPassword());
+    if (customers.size() == 1) {
+      LoginResponse loginResponse = new LoginResponse();
+      Customer currentUser = customers.get(0);
+      loginResponse.setId(currentUser.getId());
+      loginResponse.setUsername(currentUser.getUsername());
+      loginResponse.setEmail(currentUser.getEmail());
+      loginResponse.setPhoneNumber(currentUser.getPhoneNumber());
+      loginResponse.setPassword(loginRequest.getPassword());
+      loginResponse.setCars(currentUser.getCars());
+      return loginResponse;
+    }
+    throw new CustomerNoFoundException(CustomerEnum.ACCOUNT_OR_PASSWORD_IS_INCORRECT);
+  }
+}
