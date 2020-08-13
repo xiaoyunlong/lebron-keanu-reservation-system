e6adca05c8ed60362d48c8d70251c8402dd44b52
jenkincstang <jenkincs@163.com>
2020-08-13 18:27:00 +0800

    [#023][jenkin] feature: add exception process

--- src/main/java/com/oocl/reservationsystem/exception/parking/CarNotFoundException.java
+++ src/main/java/com/oocl/reservationsystem/exception/parking/CarNotFoundException.java
@@ -1,5 +1,21 @@
 package com.oocl.reservationsystem.exception.parking;
 
+import com.oocl.reservationsystem.enums.parking.CarEnum;
+
 public class CarNotFoundException extends RuntimeException {
 
+  private String message;
+
+  public CarNotFoundException() {
+
+  }
+
+  public CarNotFoundException(CarEnum carEnum) {
+    this.message = carEnum.getMessage();
+  }
+
+  @Override
+  public String getMessage() {
+    return message;
+  }
 }
